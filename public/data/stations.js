831b82f7671e7a59e62305d60205dea4ff43e5fa
brontosimungo <brontosimungo@outlook.co.id>
2025-05-29 04:34:57 +0700

    Create stations.js

---
+++ public/data/stations.js
@@ -0,0 +1,222 @@
+const stations = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "Rangkasbitung",
+        code: "RK"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.25153216983001, -6.352653602595517]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Citeras",
+        code: "CTR"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.3326800666184, -6.33475860707869]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Maja",
+        code: "MJ"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.39653385009929, -6.332319947755879]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Cikoya",
+        code: "CKY",
+        "note": "Berhenti Sesuai Sb"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.41178218756794, -6.335556384632846]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Tigaraksa",
+        code: "TGS"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.43419786661822, -6.328651297719585]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Tenjo",
+        code: "TEJ",
+        "note": "Berhenti Sesuai Sb"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.46165623778293, -6.327284987533221]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Daru",
+        code: "DAR"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.49242845471764, -6.3380073481707235]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Cilejit",
+        code: "CJT",
+        "note": "Berhenti Sesuai Sb"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.50957548522325, -6.354338088636527]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Parungpanjang",
+        code: "PRP"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.56986717482634, -6.344067067854271]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Cicayur",
+        code: "CC",
+        "note": "Berhenti Sesuai Sb"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.6189240639803, -6.329516056022527]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Cisauk",
+        code: "CSK"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.64165052031788, -6.324421929217029]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Serpong",
+        code: "SRP"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.6655547063112, -6.320036867931609]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Rawabuntu",
+        code: "RU",
+        "note": "Berhenti Sesuai Sb"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.67630098195961, -6.314822537604881]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Sudimara",
+        code: "SDM"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.71279105312398, -6.296742183297029]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Jurangmangu",
+        code: "JMU",
+        "note": "Berhenti Sesuai Sb"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.72905772244094, -6.288608601000101]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Pondokranji",
+        code: "PDJ"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.7445653224408, -6.276460025826499]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Kebayoran",
+        code: "KBY"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.78266617351764, -6.2372931635379025]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Palmerah",
+        code: "PLM"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.79738242619676, -6.207572646630825]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Tanahabang",
+        code: "THB"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [106.81085809102169, -6.185667527250411]
+      }
+    }
+  ]
+};
+
+module.exports = stations;
