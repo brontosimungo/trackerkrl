35a0c5d8b542d759dc991e895e65c1355159eaf1
brontosimungo <brontosimungo@outlook.co.id>
2025-05-30 10:57:39 +0700

    Add files via upload

---
+++ data/relasiKA.js
@@ -0,0 +1,616 @@
+const relasiKA = {
+    "KA1606": [
+      "Parungpanjang-Rangkasbitung"
+    ],
+    "KA1612F": [
+      "Parungpanjang-Tigaraksa"
+    ],
+    "KA1613": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1617": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1619": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1631F": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1643": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1655F": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1663F": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1671": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1679F": [
+      "Parungpanjang-Serpong"
+    ],
+    "KA1695": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1705": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1713": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1717": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1721": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1731": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1739": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1747": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1751": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1755": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1759": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1763": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1767": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1773": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1781": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1789": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1797": [
+      "Parungpanjang-Tanahabang"
+    ],
+    "KA1621": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1625": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1629": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1633": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1637": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1641": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1645": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1649": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1653": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1657": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1661": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1665": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1669": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1673": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1677": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1681": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1685": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1689": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1693": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1701": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1703": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1707": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1711": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1715": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1719": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1723": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1725": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1729": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1733": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1737": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1741": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1745": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1749": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1753": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1757": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1761": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1765": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1769": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1771": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1775": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1779": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1783": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1787": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1791": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1795": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1799": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1801": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1805": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1809": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1813": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1815": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1819": [
+      "Rangkasbitung-Tanahabang"
+    ],
+    "KA1817": [
+      "Rangkasbitung-Serpong"
+    ],
+    "KA1821": [
+      "Rangkasbitung-Serpong"
+    ],
+    "KA1825": [
+      "Rangkasbitung-Serpong"
+    ],
+    "KA1823": [
+      "Rangkasbitung-Parungpanjang"
+    ],
+    "KA1831": [
+      "Rangkasbitung-Parungpanjang"
+    ],
+    "KA1602": [
+      "Serpong-Rangkasbitung"
+    ],
+    "KA1610": [
+      "Serpong-Rangkasbitung"
+    ],
+    "KA1615": [
+      "Serpong-Tanahabang"
+    ],
+    "KA1623": [
+      "Serpong-Tanahabang"
+    ],
+    "KA1635": [
+      "Serpong-Tanahabang"
+    ],
+    "KA1647F": [
+      "Serpong-Tanahabang"
+    ],
+    "KA1627F": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1639F": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1651": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1659": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1667": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1675": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1683": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1691": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1699": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1743": [
+      "Serpong-Tanahabang"
+    ],
+    "KA1777": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1785": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1793": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1803": [
+      "Tigaraksa-Tanahabang"
+    ],
+    "KA1811": [
+      "Tigaraksa-Parungpanjang"
+    ],
+    "KA1632F": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1640F": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1648F": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1656F": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1664F": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1672": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1684": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1692": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1696": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1700": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1706": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1714": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1720": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1726": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1730": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1738": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1742": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1750": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1758": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1766": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1772": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1788": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1796": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1806": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1820": [
+      "Tanahabang-Parungpanjang"
+    ],
+    "KA1614": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1616": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1618": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1622": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1626": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1630": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1634": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1638": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1642": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1646": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1650": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1654": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1658": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1662": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1666": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1670": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1674": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1678": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1682": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1686": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1690": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1694": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1702": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1704": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1708": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1712": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1716": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1718": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1722": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1724": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1728": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1732": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1736": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1740": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1744": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1748": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1752": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1756": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1760": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1764": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1768": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1770": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1774": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1778": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1782": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1786": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1790": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1792": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1794": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1798": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1802": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1804": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1810": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1814": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1816": [
+      "Tanahabang-Rangkasbitung"
+    ],
+    "KA1628F": [
+      "Tanahabang-Serpong"
+    ],
+    "KA1800": [
+      "Tanahabang-Serpong"
+    ],
+    "KA1620": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1624": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1636": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1644": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1652": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1660": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1668": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1746": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1754": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1762": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1776": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1780": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1784": [
+      "Tanahabang-Tigaraksa"
+    ],
+    "KA1676": [
+      "Tanahabang-Manggarai"
+    ],
+    "KA1727": [
+      "Manggarai-Tanahabang"
+    ],
+    "KA1807": [
+      "Tigaraksa-Serpong"
+    ]
+  };
+  
+  export default relasiKA;
